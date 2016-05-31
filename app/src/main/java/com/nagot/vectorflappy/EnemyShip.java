0b4ba7ffee24e0ff9b456d799cb805ebf95e025a
Ian Nagot <inagot21@gmail.com>
2016-05-31 16:02:20 +0100

    Add new class to the enemy ship

---
+++ app/src/main/java/com/nagot/vectorflappy/EnemyShip.java
@@ -0,0 +1,16 @@
+package com.nagot.vectorflappy;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Nagot on 31/05/2016.
+ */
+public class EnemyShip {
+    private Bitmap bitmap;
+    private int x, y;
+    private int speed = 1;
+    private int maxX;
+    private int minX;
+    private int maxY;
+    private int minY;
+}
