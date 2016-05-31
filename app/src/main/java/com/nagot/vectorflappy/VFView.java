a6593a5b69870e825cc0e876d97a3a9ca9784283
Ian Nagot <inagot21@gmail.com>
2016-05-31 11:36:09 +0100

    Implement the following methods to VFView class: draw, update and control

--- app/src/main/java/com/nagot/vectorflappy/VFView.java
+++ app/src/main/java/com/nagot/vectorflappy/VFView.java
@@ -1,6 +1,10 @@
 package com.nagot.vectorflappy;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /**
@@ -10,9 +14,16 @@ public class VFView extends SurfaceView implements Runnable {
 
     volatile boolean playing;
     Thread gameThread = null;
+    private PlayerShip player;
+    private Paint paint;
+    private Canvas canvas;
+    private SurfaceHolder ourHolder;
 
     public VFView(Context context) {
         super(context);
+        ourHolder = getHolder();
+        paint = new Paint();
+        player = new PlayerShip(context);
     }
 
     @Override
@@ -25,14 +36,31 @@ public class VFView extends SurfaceView implements Runnable {
     }
 
     private void update() {
-
+        player.update();
     }
 
     private void draw() {
+        if(ourHolder.getSurface().isValid()) {
+            canvas = ourHolder.lockCanvas();
 
+            canvas.drawColor(Color.argb(255,0,0,0));
+
+            canvas.drawBitmap(
+                    player.getBitmap(),
+                    player.getX(),
+                    player.getY(),
+                    paint);
+
+            ourHolder.unlockCanvasAndPost(canvas);
+        }
     }
 
     private void control() {
+        try {
+            gameThread.sleep(17);
+        } catch (InterruptedException e) {
+
+        }
     }
 
     public void pause() {
