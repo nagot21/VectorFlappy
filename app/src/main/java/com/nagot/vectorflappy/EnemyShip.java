89d607ead3ad21e0ec6e3a1d7874ef41f47a25aa
Ian Nagot <inagot21@gmail.com>
2016-05-31 16:08:24 +0100

    Add constructor of the class and enemy.png to the drawable folder

--- app/src/main/java/com/nagot/vectorflappy/EnemyShip.java
+++ app/src/main/java/com/nagot/vectorflappy/EnemyShip.java
@@ -1,6 +1,10 @@
 package com.nagot.vectorflappy;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.Random;
 
 /**
  * Created by Nagot on 31/05/2016.
@@ -13,4 +17,30 @@ public class EnemyShip {
     private int minX;
     private int maxY;
     private int minY;
+
+    public EnemyShip(Context context, int screenX, int screenY) {
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.enemy);
+        maxX = screenX;
+        maxY = screenY;
+        minX = 0;
+        minY = 0;
+
+        Random generator = new Random();
+        speed = generator.nextInt(6) + 10;
+
+        x = screenX;
+        y = generator.nextInt(maxY) - bitmap.getHeight();
+    }
+
+    public Bitmap getBitmap(){
+        return bitmap;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
